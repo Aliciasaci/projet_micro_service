dd3803fc2622a86a1759a787db2b9e83d184bcb7
alicia <alicia@sopress.net>
2023-07-04 16:11:25 +0200

    update docker

--- recette-api/src/app.service.ts
+++ recette-api/src/app.service.ts
@@ -1,4 +1,3 @@
-//fichier qui implémente le service prisma et écrit une fonction pour le CRUD recette
 import { Injectable } from '@nestjs/common';
 import { Recette } from './stubs/recette/v1alpha/recette';
 import { PrismaService } from './prisma.service';
@@ -8,125 +7,58 @@ import { Prisma } from '@prisma/client';
 export class AppService {
   constructor(private prisma: PrismaService) { }
 
-  /**
-   * 
-   * @param data 
-   * @returns 
-   */
   async create(data: Prisma.RecetteCreateInput): Promise<Recette> {
-    const result: PrismaRecette = await this.prisma.recette.create({ data });
-    const recette: Recette = {
-      id: result.id,
-      nom: result.nom,
-      description: result.description,
-      ingredients: result.ingredients,
-      instructions: result.instructions,
-      temps_preparation: result.temps_preparation,
-      temps_cuisson: result.temps_cuisson,
-    };
-    return recette;
+    const result: Recette = await this.prisma.recette.create({ data });
+    return result;
   }
 
-  /**
-   * 
-   * @returns tableau de recette
-   */
   async findAll(): Promise<Recette[]> {
-    const results: PrismaRecette[] = await this.prisma.recette.findMany();
-    const recettes: Recette[] = results.map((result: PrismaRecette) => ({
-      id: result.id,
-      nom: result.nom,
-      description: result.description,
-      ingredients: result.ingredients,
-      instructions: result.instructions,
-      temps_preparation: result.temps_preparation,
-      temps_cuisson: result.temps_cuisson,
-    }));
-    return recettes;
+    const results: Recette[] = await this.prisma.recette.findMany();
+    return results;
   }
 
-  /**
-   * 
-   * @param id 
-   * @returns Objet recette
-   */
   async findById(id: number): Promise<Recette> {
-    const result: PrismaRecette | null = await this.prisma.recette.findUnique({
+    const result: Recette | null = await this.prisma.recette.findUnique({
       where: { id },
     });
     if (!result) {
       throw new Error(`Recette with ID ${id} not found`);
     }
-    const recette: Recette = {
-      id: result.id,
-      nom: result.nom,
-      description: result.description,
-      ingredients: result.ingredients,
-      instructions: result.instructions,
-      temps_preparation: result.temps_preparation,
-      temps_cuisson: result.temps_cuisson,
-    };
-    return recette;
+    return result;
   }
 
-  /**
-   * 
-   * @param id 
-   * @param data 
-   * @returns 
-   */
   async update(id: number, data: Prisma.RecetteUpdateInput): Promise<Recette> {
-    const result: PrismaRecette | null = await this.prisma.recette.update({
+    const result: Recette | null = await this.prisma.recette.update({
       where: { id },
       data,
     });
     if (!result) {
       throw new Error(`Recette with ID ${id} not found`);
     }
-    const recette: Recette = {
-      id: result.id,
-      nom: result.nom,
-      description: result.description,
-      ingredients: result.ingredients,
-      instructions: result.instructions,
-      temps_preparation: result.temps_preparation,
-      temps_cuisson: result.temps_cuisson,
-    };
-    return recette;
+    return result;
   }
 
-
   async findByName(nom: string): Promise<Recette> {
-    return this.prisma.recette.findFirst({
+    const result: Recette | null = await this.prisma.recette.findFirst({
       where: {
         nom: {
           equals: nom,
         },
       },
     });
+    if (!result) {
+      throw new Error(`Recette with name ${nom} not found`);
+    }
+    return result;
   }
 
-  /**
-   * 
-   * @param id 
-   * @returns 
-   */
   async delete(id: number): Promise<Recette> {
-    const result: PrismaRecette | null = await this.prisma.recette.delete({
+    const result: Recette | null = await this.prisma.recette.delete({
       where: { id },
     });
     if (!result) {
       throw new Error(`Recette with ID ${id} not found`);
     }
-    const recette: Recette = {
-      id: result.id,
-      nom: result.nom,
-      description: result.description,
-      ingredients: result.ingredients,
-      instructions: result.instructions,
-      temps_preparation: result.temps_preparation,
-      temps_cuisson: result.temps_cuisson,
-    };
-    return recette;
+    return result;
   }
 }
