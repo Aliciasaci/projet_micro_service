eb6c70ccbeeea320a6328b3217d84819499e1467
Alicia Saci <45595003+Aliciasaci@users.noreply.github.com>
2023-07-05 12:04:43 +0200

    Update grpc.config.js

--- recette-api/src/grpc.config.js
+++ recette-api/src/grpc.config.js
@@ -8,7 +8,7 @@ var nestjs_grpc_reflection_1 = require("nestjs-grpc-reflection");
 exports.grpcConfig = (0, nestjs_grpc_reflection_1.addReflectionToGrpcConfig)({
     transport: microservices_1.Transport.GRPC,
     options: {
-        url: '0.0.0.0:6000',
+        url: '0.0.0.0:4000',
         package: recette_1.RECETTE_V1ALPHA_PACKAGE_NAME,
         protoPath: (0, path_1.join)(__dirname, '../src/proto/recette/v1alpha/recette.proto'),
     },
