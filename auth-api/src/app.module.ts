8171f4d369114b3598670b2d817b2d48376a0032
maithi-trandiep <thi.tdm04@gmail.com>
2023-07-12 23:59:27 +0200

    WIP auth-api

--- auth-api/src/app.module.ts
+++ auth-api/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 5432,
+      database: 'app',
+      username: 'root',
+      password: null,
+      entities: ['dist/**/*.entity.{ts,js}'],
+      synchronize: true,
+    }),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
